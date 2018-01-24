1049fb05ede8d93e4d743fd79d65d8842c01f227
Skylar Dunn-Lubin <skylar.dunnlubin@gmail.com>
2018-01-24 23:45:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-const newObj = Object.assign({}, object,[key])
+const newObj = Object.assign({}, object)
+delete newObj
 }
